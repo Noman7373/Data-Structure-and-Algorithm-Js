f1ca03449c3706eb609e5f0c8d0e240b3fdf1654
NomanAhmed <officialnoman515@gmail.com>
2024-10-13 13:48:02 +0300

    Practice QucikSort Algorithm

--- Day 16 Quick Sort/app.js
+++ Day 16 Quick Sort/app.js
@@ -162,3 +162,120 @@ let n = [
 ];
 quickSortArrayInterval(n, 0, n.length - 1);
 console.log(n); //Output: [(1, 2), (2, 3), (3, 4), (4, 5)]
+
+// Problem: Use the quicksort partition approach to find the k-th smallest element in the array.
+//Input Array: [10, 4, 5, 8, 6, 11, 26]
+// Output 6 (3rd smallest element)
+
+function sortQuick(numArray, low, high, kSmallest) {
+  if (low <= high) {
+    let pivotIndex = partitionPivot(numArray, low, high);
+    if (pivotIndex === numArray.length - kSmallest) {
+      return numArray[pivotIndex];
+    } else if (pivotIndex > numArray.length - kSmallest) {
+      return sortQuick(numArray, low, pivotIndex - 1, kSmallest);
+    } else {
+      return sortQuick(numArray, pivotIndex + 1, high, kSmallest);
+    }
+  }
+}
+
+function partitionPivot(numArray, low, high) {
+  let pivot = numArray[high];
+  let i = low - 1;
+  for (let j = low; j < high; j++) {
+    if (numArray[j] > pivot) {
+      i++;
+      [numArray[i], numArray[j]] = [numArray[j], numArray[i]];
+    }
+  }
+  [numArray[i + 1], numArray[high]] = [numArray[high], numArray[i + 1]];
+  return i + 1;
+}
+
+function findKthSmallest(numArray, kSmallest) {
+  return sortQuick(numArray, 0, numArray.length - 1, kSmallest);
+}
+
+let numArray = [10, 4, 5, 8, 6, 11, 26];
+let kSmallest = 3;
+
+console.log(
+  `The Smallest k ${kSmallest} index element is ${findKthSmallest(
+    numArray,
+    kSmallest
+  )} `
+);
+
+//Problem: Implement a randomized quicksort where the pivot is chosen randomly.
+//Input [9, 7, 5, 11, 12, 2, 14, 3, 10, 6]
+
+function Sorting(arrNum, low, high) {
+  if (low <= high) {
+    let pivot = Partition(arrNum, low, high);
+    Sorting(arrNum, low, pivot - 1);
+    Sorting(arrNum, pivot + 1, high);
+  }
+}
+
+function Partition(arrNum, low, high) {
+  let randomIndex = Math.floor(Math.random() * (high - low + 1)) + low;
+  let pivot = arrNum[randomIndex];
+
+  [arrNum[randomIndex], arrNum[high]] = [arrNum[high], arrNum[randomIndex]];
+  let i = low - 1;
+  for (let j = low; j < high; j++) {
+    if (arrNum[j] < pivot) {
+      i++;
+      [arrNum[i], arrNum[j]] = [arrNum[j], arrNum[i]];
+    }
+  }
+  [arrNum[i + 1], arrNum[high]] = [arrNum[high], arrNum[i + 1]];
+  return i + 1;
+}
+
+let arrNum = [9, 7, 5, 11, 12, 2, 14, 3, 10, 6];
+Sorting(arrNum, 0, arrNum.length - 1);
+console.log(arrNum);
+
+//  Dual-Pivot Quicksort Implementation
+// input [8, 2, 4, 1, 9, 5, 7, 6, 3]
+function dualQuickSort(Number, low, high) {
+  if (low < high) {
+    let [pivot1, pivot2] = Partition(Number, low, high);
+    dualQuickSort(Number, low, pivot1 - 1);
+    dualQuickSort(Number, pivot1 + 1, pivot2 - 1);
+    dualQuickSort(Number, pivot2 + 1, high);
+  }
+}
+
+function Partition(Number, low, high) {
+  if (Number[low] > Number[high]) {
+    [Number[low], Number[high]] = [Number[high], Number[low]];
+  }
+  let pivot1 = Number[low];
+  let pivot2 = Number[high];
+
+  let i = low + 1;
+  let j = high - 1;
+  let k = low + 1;
+
+  while (k <= j) {
+    if (Number[k] < pivot1) {
+      [Number[i], Number[k]];
+      i++;
+    } else if (Number[k] > pivot2) {
+      while (Number[j] > pivot2 && k < j) {
+        j--;
+      }
+      [Number[k], Number[j]] = [Number[j], Number[k]];
+      j--;
+    }
+    if (Number[k] < pivot1) {
+      [Number[i], Number[k]][(Number[k], Number[i])];
+      i++;
+    }
+  }
+  i++
+  
+}
